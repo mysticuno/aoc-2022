336ca78f4daba61282d616ed0782bcee26051708
Carlos Henríquez <carlosh@alum.mit.edu>
2022-12-08 08:45:53 -0500

    Day 10 part 2

--- day10/day10.py
+++ day10/day10.py
@@ -1,40 +1,18 @@
 with open('input.txt') as f:
-    '''
-    Start by figuring out the signal being sent by the CPU. The CPU has a single register, X, which starts with the value 1. It supports only two instructions:
-
-    addx V takes two cycles to complete. After two cycles, the X register is increased by the value V. (V can be negative.)
-    
-    noop takes one cycle to complete. It has no other effect.
-    '''
     x = 1
     cycle = 0
     lines = (line.strip().split() for line in f)
-    adding = False
     signals = []
-    while True:
-        try:
-            # print(f"Start of loop, state: adding:{adding} cycle:{cycle} x:{x}")
-            if adding:
-                adding = False
-                cycle += 1
-                if cycle % 20 == 0:
-                    # print(f"\tAdding to signal, state: line:{line} adding:{adding} cycle:{cycle} x:{x}")
-                    signals.append([cycle, x, cycle*x])
-                x += int(line[1])
-                continue
-            line = next(lines)
-            # print(line)
+    for line in lines:
+        cycle += 1
+        if (cycle+20) % 40 == 0:
+            signals.append(cycle*x)
+        if len(line) == 1: # noop
+            continue
+        else:
             cycle += 1
-            if cycle % 20 == 0:
-                # print(f"\tAdding to signal, state: line:{line} adding:{adding} cycle:{cycle} x:{x}")
-                signals.append([cycle, x, cycle*x])
+            if (cycle + 20) % 40 == 0:
+                signals.append(cycle*x)
+            x += int(line[1])
 
-            if len(line) == 1: #noop
-                continue
-            else: # addx <num>
-                adding = True
-        except StopIteration:
-            break
-    print(signals)
-    print([signal[2] for signal in signals if signal[0] in range(20, 221, 40)])
-    print(sum(signal[2] for signal in signals if signal[0] in range(20, 221, 40)))
+    print(sum(signals))
--- day10/day10_pt2.py
+++ day10/day10_pt2.py
@@ -1,40 +1,23 @@
 with open('input.txt') as f:
-    '''
-    Start by figuring out the signal being sent by the CPU. The CPU has a single register, X, which starts with the value 1. It supports only two instructions:
-
-    addx V takes two cycles to complete. After two cycles, the X register is increased by the value V. (V can be negative.)
-    
-    noop takes one cycle to complete. It has no other effect.
-    '''
     x = 1
     cycle = 0
     lines = (line.strip().split() for line in f)
-    adding = False
     pixels = []
-    while True:
-        try:
-            # print(f"Start of loop, state: adding:{adding} cycle:{cycle} x:{x}")
-            if adding:
-                adding = False
-                cycle += 1
-                if cycle % 20 == 0:
-                    # print(f"\tAdding to signal, state: line:{line} adding:{adding} cycle:{cycle} x:{x}")
-                    signals.append([cycle, x, cycle*x])
-                x += int(line[1])
-                continue
-            line = next(lines)
-            # print(line)
+    for line in lines:
+        if abs(x - cycle%40) <= 1:
+            pixels.append('#')
+        else:
+            pixels.append('.')
+        cycle += 1
+        if len(line) == 1: # noop
+            continue
+        else:
+            if abs(x - cycle%40) <= 1:
+                pixels.append('#')
+            else:
+                pixels.append('.')
             cycle += 1
-            if cycle % 20 == 0:
-                # print(f"\tAdding to signal, state: line:{line} adding:{adding} cycle:{cycle} x:{x}")
-                signals.append([cycle, x, cycle*x])
+            x += int(line[1])
 
-            if len(line) == 1: #noop
-                continue
-            else: # addx <num>
-                adding = True
-        except StopIteration:
-            break
-    print(signals)
-    print([signal[2] for signal in signals if signal[0] in range(20, 221, 40)])
-    print(sum(signal[2] for signal in signals if signal[0] in range(20, 221, 40)))
+    for i in range(6):
+        print(''.join(pixels[i*40:(i+1)*40]))
